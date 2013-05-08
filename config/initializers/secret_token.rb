d084b35dbd7def7b5b305729077624c2e3bada86
alexevangelisti <alexevangelisti@me.com>
2013-05-08 21:28:33 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HannybalApp::Application.config.secret_token = '008cd1ef4758f8d53150677c833ae36f7c7e0c0b6c8838a488d5a220cc42640c77c736f77b54971bb6e70a4f3296c01250d3be5657fe7f1480e7d957bd57029b'
